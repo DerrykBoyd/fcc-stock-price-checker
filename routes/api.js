35c2e2788aae6c9993ab642c14f2590a0e00dfd9
dboydgit <derrykboyd@gmail.com>
2019-03-28 01:19:36 -0600

    user stories 1-6 complete

--- routes/api.js
+++ routes/api.js
@@ -13,32 +13,78 @@ var MongoClient = require('mongodb');
 const rp = require('request-promise');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const COLLECTION_NAME = 'stockPriceChecker';
 const STOCK_API = 'https://api.iextrading.com/1.0';
 
 let getStockPrice = ticker => {
-  return rp(`${STOCK_API}/stock/${ticker}/price`, {json:true})
+  return rp(`${STOCK_API}/stock/${ticker}/price`, { json: true })
     .then(body => {
-      console.log(`Ticker: ${ticker}, Price: ${body}`);
       return body;
     })
     .catch(err => {
-      console.log(err);
+      console.log('Stock API Error');
       return err;
     })
+}
+
+let addLike = (ticker, ip, collection) => {
+  return collection.updateOne({ticker: ticker}
+    , {$addToSet: {ipLikes: ip}}
+    , {upsert: true});
+}
+
+let getLikes = (ticker, collection) => {
+  return collection.findOne({ticker: ticker}).then(function(doc) {
+    if (!doc) return 0;
+    else return doc.ipLikes.length;
+  })
 }
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(async function (req, res){
-      let ticker = req.query.stock;
-      getStockPrice(ticker).then(body => {
-        res.send({
-          stock: ticker,
-          price: body.toString(),
-          likes: '// TODO'
-        })
-      });
+    .get(async function (req, res) {
+      MongoClient.connect(CONNECTION_STRING, async function (err, db) {
+        if (err) console.log('Failed to connect to db' + err);
+        else console.log('Connected to DB');
+        let collection = db.collection(COLLECTION_NAME);
+        console.log(req.query);
+        console.log(req.ip);
+        let ticker = req.query.stock;
+        let ip = req.ip;
+        let stockData = {};
+        let results = [];
+        if (Array.isArray(ticker)) {
+          let likes = [];
+          for (let tick in ticker) {
+            if (req.query.like) await addLike(ticker[tick], ip, collection);
+            likes[tick] = await getLikes(ticker[tick], collection);
+            await getStockPrice(ticker[tick]).then(body => {
+              results.push({
+                stock: ticker[tick],
+                price: body.toString(),
+              })
+            })
+          }
+          console.log(likes);
+          results[0].rel_likes = likes[0] - likes[1];
+          results[1].rel_likes = likes[1] - likes[0];
+          stockData.stockData = results;
+          res.send(stockData);
+        } else {
+          if (req.query.like) await addLike(ticker, ip, collection);
+          getStockPrice(ticker).then(body => {
+            getLikes(ticker, collection).then(likes => {
+              console.log(likes);
+              res.send({
+                stock: ticker,
+                price: body.toString(),
+                likes: likes
+              })
+            })
+          });
+        }
+      })
     });
-    
+
 };
